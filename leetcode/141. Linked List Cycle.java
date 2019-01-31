47aea6e653714b143953006defa07d6dd29ffec4
Siqi Dong <siqidong2015@gmail.com>
2019-01-31 00:40:59 -0800

    update 086

---
+++ leetcode/141. Linked List Cycle.java
@@ -0,0 +1,23 @@
+/*
+Fast - Slow pointer
+
+TC: O(n)
+SC: O(1)
+*/
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        if(head==null || head.next==null) {
+            return false;
+        }
+        ListNode slow = head;
+        ListNode fast = head.next;
+        while(fast!=null && fast.next!=null) {
+            if(slow==fast) {
+                return true;
+            }
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return false;
+    }
+}
