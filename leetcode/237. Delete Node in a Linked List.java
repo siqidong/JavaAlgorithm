51c78c92938e171e4c044f7b3fad00c59004ecb9
Siqi Dong <siqidong2015@gmail.com>
2019-01-31 02:49:19 -0800

    update 237

---
+++ leetcode/237. Delete Node in a Linked List.java
@@ -0,0 +1,12 @@
+/*
+fei chang zhi zhang...
+
+TC: O(1)
+SC: O(1)
+*/
+class Solution {
+    public void deleteNode(ListNode node) {
+        node.val = node.next.val;
+        node.next = node.next.next;
+    }
+}
